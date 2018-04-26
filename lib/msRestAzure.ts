77ba3a90c3b71c021f35861b4d80f518fa8df56e
Dan Schulte <daschult@microsoft.com>
2018-04-26 15:44:49 -0700

    Add type and function to msRestAzure.ts

--- lib/msRestAzure.ts
+++ lib/msRestAzure.ts
@@ -6,4 +6,7 @@ import Constants from "./util/constants";
 import { CloudError, CloudErrorMapper } from "./cloudError";
 import { BaseResource, BaseResourceMapper } from "./baseResource";
 import { CognitiveServicesCredentials } from "./credentials/cognitiveServicesCredentials";
-export { AzureServiceClient, Constants, CloudError, CloudErrorMapper, BaseResource, BaseResourceMapper, CognitiveServicesCredentials };
+import { DefaultAzureHttpPipelineOptions, createDefaultAzureHttpPipeline } from "./azureHttpPipeline";
+export { AzureServiceClient, Constants, CloudError, CloudErrorMapper, BaseResource,
+  BaseResourceMapper, CognitiveServicesCredentials, DefaultAzureHttpPipelineOptions,
+  createDefaultAzureHttpPipeline };
