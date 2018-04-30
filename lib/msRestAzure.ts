c0d2c2a8e38da196c6c2ad5a7ff3fe40fcb9291b
Dan Schulte <daschult@microsoft.com>
2018-04-30 09:38:00 -0700

    Add AzureServiceClientOptions to msRestAzure.ts

--- lib/msRestAzure.ts
+++ lib/msRestAzure.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import { AzureServiceClient } from "./azureServiceClient";
+import { AzureServiceClient, AzureServiceClientOptions } from "./azureServiceClient";
 import Constants from "./util/constants";
 import { CloudError, CloudErrorMapper } from "./cloudError";
 import { BaseResource, BaseResourceMapper } from "./baseResource";
 import { CognitiveServicesCredentials } from "./credentials/cognitiveServicesCredentials";
-export { AzureServiceClient, Constants, CloudError, CloudErrorMapper, BaseResource,
+export { AzureServiceClient, AzureServiceClientOptions, Constants, CloudError, CloudErrorMapper, BaseResource,
   BaseResourceMapper, CognitiveServicesCredentials };
